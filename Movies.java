07d6ba0c6e98d4a7f5ef59f84478690f67a25b16
DuranDrop-Dev <127360444+DuranDrop-Dev@users.noreply.github.com>
2023-06-11 18:16:04 -0700

    Initial Commit

---
+++ Movies.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.util.LinkedList;
+import javax.swing.*;
+
+public class Movies {
+    public static LinkedList<String> movieList = new LinkedList<>();
+    public static JLabel movieLabel = new JLabel("", SwingConstants.CENTER);
+    public static void main(String []args) {
+        // Components
+        JFrame frame = new JFrame("MovieLinkedList");
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new OverlayLayout(mainPanel));
+        JPanel contentPanel = new JPanel(new GridBagLayout());
+
+        JButton addMovie = new JButton("Add");
+        JButton deleteMovie = new JButton("Delete");
+        JButton updateMovie = new JButton("Update");
+
+        // Layout
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10,10,10,10);
+
+        mainPanel.add(movieLabel);
+        movieLabel.setAlignmentY(0.0f);
+        movieLabel.setAlignmentX(0.5f);
+
+        mainPanel.add(contentPanel);
+        contentPanel.setAlignmentY(0.1f);
+        contentPanel.setAlignmentX(0.5f);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        contentPanel.add(addMovie, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        contentPanel.add(deleteMovie, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        contentPanel.add(updateMovie, gbc);
+
+        frame.setSize(new Dimension(600, 200));
+        frame.setContentPane(mainPanel);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        // Action Listeners
+        addMovie.addActionListener(e-> addList());
+        deleteMovie.addActionListener(e-> deleteList());
+        updateMovie.addActionListener(e -> updateList());
+    }
+    public static void addList() {
+        String str = JOptionPane.showInputDialog("Add Movie");
+        movieList.add(str);
+        movieLabel.setText(movieList.toString());
+    }
+    public static void deleteList() {
+        String str = JOptionPane.showInputDialog("Delete Movie");
+        movieList.removeIf(el -> el.equals(str));
+        movieLabel.setText(movieList.toString());
+    }
+    public static void updateList() {
+        String str = JOptionPane.showInputDialog("Update Movie");
+        for (String el : movieList) {
+            if (el.equals(str)) {
+                String newStr = JOptionPane.showInputDialog("New Movie");
+                int index = movieList.indexOf(el);
+                movieList.set(index, newStr);
+            }
+        }
+        movieLabel.setText(movieList.toString());
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# LinkedList
+<h2>Add, Delete, Update</h2>
+<li>Sample of a LinkedList in Java with UI components</li>
